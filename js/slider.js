0cfb907c4843f8a530c58a1f7d96c4ead1a6ba58
Misha-1996 <97362343+Misha-1996@users.noreply.github.com>
2022-01-31 11:27:24 +0300

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,13 @@
+
+
+// const text = document.querySelector('.city_txt1');
+// const imagess = document.querySelector('project-content-img1');
+// const images = [
+//     {
+//         text: text.innerText('Sochi Thieves');
+//         img:
+//     }
+// ]
+
+
+
